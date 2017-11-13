0938968e576509e0c099c794846bc595d85f36e8
solufemi91 <stevenoo@spartaglobal.com>
2017-11-13 22:55:15 +0000

    tidied up the code

--- js/main.js
+++ js/main.js
@@ -15,12 +15,12 @@ var randomColor = 0;
 var counter = 0;
 var id = 0;
 var indexCounter = -1;
+var $displayMessage = $('.displayMessage');
 
 /// player making their choice about what to pick
 $boxes.click(function(){
   var boxIClicked = $(this).attr('id');
   $(this).css('backgroundColor',boxIClicked);
-
   playersChoice.push(boxIClicked);
   indexCounter++
   compareArrays();
@@ -31,21 +31,16 @@ $boxes.click(function(){
 
 $startButton.click(function(){
 
-  indexCounter = -1;
-  playersChoice = [];
-  computersChoice = [];
-  $('.boxes').css('backgroundColor','white');
+  resetter();
   var id = setInterval(frame,500);
   function frame(){
     if(randomColor != 0){
       randomBox.style.backgroundColor = 'white';
       randomColor = 0;
       counter++;
-
-
       checkCounter();
 
-    } else{
+    } else {
     randomBox = boxes[Math.floor(Math.random() * 4)];
     randomColor = randomBox.getAttribute('id');
     computersChoice.push(randomColor);
@@ -58,7 +53,6 @@ $startButton.click(function(){
     if(counter == 4){
       clearInterval(id);
       counter = 0;
-
     }
   }
 
@@ -67,14 +61,23 @@ $startButton.click(function(){
 })
 
 function compareArrays(){
-  //if this value is the same as the value in the other array
-  //console log 'correct'
-  if(computersChoice[indexCounter] === playersChoice[indexCounter]){
-  console.log('correct')
-} else {
-  console.log('incorrect');
 
+  if(computersChoice[indexCounter] === playersChoice[indexCounter]){
+  // console.log('correct');
+  $displayMessage.html('Correct');
+  } else {
+  console.log('incorrect');
+  $displayMessage.html('Incorrect');
+  }
 }
+
+function resetter(){
+  $displayMessage.html('correct/incorrect');
+  indexCounter = -1;
+  playersChoice = [];
+  computersChoice = [];
+  $('.boxes').css('backgroundColor','white');
+
 }
 
 
