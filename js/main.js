82590fdae3d4416a54867c6fa72f49a77d643b73
Steve Olufemi <solufemi-olukanni@hotmail.com>
2018-10-25 21:34:21 +0200

    refactoring started

--- js/main.js
+++ js/main.js
@@ -1,171 +1,157 @@
 $(function(event) {
 
-var $startButton = $('#startButton');
-var boxes = document.getElementsByClassName('boxes');
-var $boxes = $('.boxes');
-var $redBox = $('#red');
-var $blueBox = $('#blue');
-var $greenBox = $('#green');
-var $yellowBox = $('#yellow');
-var fourColours = ['red','blue','green','yellow'];
-var playersChoice = [];
-var computersChoice = [];
-var randomBox = 0;
-var randomColor = 0;
-var counter = 0;
-var id = 0;
-var indexCounter = -1;
-var clickCounter = 0
-var correctClicks = 0;
-var incorrectClicks = 0;
-var $congratulationsMessage = $('#congratulations');
-var $displayMessage = $('.displayMessage');
-var $promptToEnter = $('#promptToEnter');
-var $numberOfClicksMade = $('#numberOfClicksMade');
-var level = 4;
-var $scoreboard = $('#scoreboard')
-var totalscore = 0;
-var $clickRegister = $('#clickRegister')
-var boxIClicked = 0;
-var clickRegCount = 0;
+  var $startButton = $('#startButton');
+  var boxes = document.getElementsByClassName('boxes');
+  var $boxes = $('.boxes');
+  var $redBox = $('#red');
+  var $blueBox = $('#blue');
+  var $greenBox = $('#green');
+  var $yellowBox = $('#yellow');
+  var fourColours = ['red','blue','green','yellow'];
+  var playersChoice = [];
+  var computersChoice = [];
+  var randomBox = 0;
+  var randomColor = 0;
+  var counter = 0;
+  var id = 0;
+  var indexCounter = -1;
+  var clickCounter = 0
+  var correctClicks = 0;
+  var incorrectClicks = 0;
+  var $congratulationsMessage = $('#congratulations');
+  var $displayMessage = $('.displayMessage');
+  var $promptToEnter = $('#promptToEnter');
+  var $numberOfClicksMade = $('#numberOfClicksMade');
+  var level = 4;
+  var $scoreboard = $('#scoreboard')
+  var totalscore = 0;
+  var $clickRegister = $('#clickRegister')
+  var boxIClicked = 0;
+  var clickRegCount = 0;
 
-/// player making their choice about what to pick
-$boxes.click(function(){
-  if(clickCounter < level && incorrectClicks == 0){
-  var boxIClicked = $(this).attr('id');
-  $(this).css('backgroundColor',boxIClicked);
+/// player making their choice about what to pick. This code changes highlights the box that is clicked. This block of
+// code is excuted each time the player clicks a box
+  $boxes.click(function(){
+    if(clickCounter < level && incorrectClicks == 0){
+      var boxIClicked = $(this).attr('id');
+      $(this).css('backgroundColor',boxIClicked);
 
-  // insert code to make the the click register flashes
-  function clickreg() {
-    var id10 = setInterval(frame,200);
+  // insert code to register the clicks that have been made. I.e. if a blue box is clicked, the user is informed that he clicked blue
+      function clickreg() {
+        var id10 = setInterval(frame,200);
 
-    function frame(){
-      if($clickRegister.html() != ''){
-        $clickRegister.html('')
-        clickRegCount++
-        checkClickReg()
-      }
-      else {
-      $clickRegister.html(boxIClicked);
-      $clickRegister.css('color',boxIClicked);
-      }
-    }
-    function checkClickReg(){
-      if(clickRegCount == 1){
-      clearInterval(id10);
-      clickRegCount = 0;
-      }
-    }
+        function frame(){
+          // if text is present, clear it
+          if($clickRegister.html() != ''){
+            $clickRegister.html('')
+            clickRegCount++
+            checkClickReg()
+          }
+          // if text is not present, add text to indicate which box was clicked
+          else {
+            $clickRegister.html(boxIClicked);
+            $clickRegister.css('color',boxIClicked);
+          }
+        }
+        // this insures that the click register only flashes once per click
+        function checkClickReg(){
+          if(clickRegCount == 1){
+            clearInterval(id10);
+            clickRegCount = 0;
+          }
+        }
 
-  };
-  clickreg();
-  // $clickRegister.html(boxIClicked);
-  // $clickRegister.css('color',boxIClicked);
-  playersChoice.push(boxIClicked);
+      };
+      clickreg();
+      // this kees a track of the colours that was selected by the user
+      playersChoice.push(boxIClicked);
   // each click create an index value for the two arrays when they are compared
 
-    indexCounter++
-    clickCounter++
-    compareArrays();
-    console.log(playersChoice);
-  }
-})
+      indexCounter++
+      clickCounter++
+      compareArrays();
+    }
+  })
 
 //random colors being genrated four times when the start button is clicked
 
-$startButton.click(function(){
+  $startButton.click(function(){
 
-  resetter();
-  var id = setInterval(frame,500);
-  function frame(){
-    if(randomColor != 0){
-      randomBox.style.backgroundColor = 'white';
-      randomColor = 0;
-      counter++;
-      checkCounter();
+    resetter();
+    var id = setInterval(frame,500);
+    function frame(){
+      if(randomColor != 0){
+        randomBox.style.backgroundColor = 'white';
+        randomColor = 0;
+        counter++;
+        checkCounter();
 
-    } else {
-    randomBox = boxes[Math.floor(Math.random() * 4)];
-    randomColor = randomBox.getAttribute('id');
-    computersChoice.push(randomColor);
-    console.log(computersChoice);
-    randomBox.style.backgroundColor = randomColor;
+      } else {
+        randomBox = boxes[Math.floor(Math.random() * 4)];
+        randomColor = randomBox.getAttribute('id');
+        computersChoice.push(randomColor);
+        console.log(computersChoice);
+        randomBox.style.backgroundColor = randomColor;
     /// wait a few milli seconds and then change it to white
 
-  }
+      }
   // once four flashes has happened, the timer is stopped
-  function checkCounter(){
-    if(counter == level){
-      clearInterval(id);
-      counter = 0;
-      $promptToEnter.html('Copy the pattern')
+      function checkCounter(){
+        if(counter == level){
+          clearInterval(id);
+          counter = 0;
+          $promptToEnter.html('Copy the pattern')
+        }
+      }
     }
-  }
 
-  }
-
-})
+  })
 
 // this function is called each time the player makes a choice. If a selection is correct, a
 // message is displayed saying that it is correct
-function compareArrays(){
+  function compareArrays(){
 
-  if(computersChoice[indexCounter] === playersChoice[indexCounter]){
-    correctClicks++;
-    if(correctClicks==level){
-      $congratulationsMessage.html("Level " + level + " passed! Click Start to proceed to next level");
-      level++;
-      totalscore += correctClicks;
-      $scoreboard.html('Score: ' + totalscore)
+    if(computersChoice[indexCounter] === playersChoice[indexCounter]){
+      correctClicks++;
+      if(correctClicks==level){
+        $congratulationsMessage.html("Level " + level + " passed! Click Start to proceed to next level");
+        level++;
+        totalscore += correctClicks;
+        $scoreboard.html('Score: ' + totalscore)
+      }
+
+      else if (clickCounter == level && correctClicks < level) {
+        $congratulationsMessage.html("Unfortunately you lost this round. Click start to try again")
+      }
     }
 
-    else if (clickCounter == level && correctClicks < level) {
+    else if (computersChoice[indexCounter] !== playersChoice[indexCounter]){
+      incorrectClicks++
       $congratulationsMessage.html("Unfortunately you lost this round. Click start to try again")
+      $displayMessage.html('Score ' + correctClicks);
+
+    }
+
+
+    else {
       // $displayMessage.html('Score ' + correctClicks);
       // $numberOfClicksMade.html('number of clicks made ' + clickCounter);
     }
-    // $displayMessage.html('Score ' +  correctClicks);
-    // $numberOfClicksMade.html('number of clicks made ' + clickCounter);
-  }
-
-  else if (computersChoice[indexCounter] !== playersChoice[indexCounter]){
-    incorrectClicks++
-    $congratulationsMessage.html("Unfortunately you lost this round. Click start to try again")
-    $displayMessage.html('Score ' + correctClicks);
-    // $numberOfClicksMade.html('number of clicks made ' + clickCounter);
 
   }
-  // else if (clickCounter == level && correctClicks < level){
-  //
-  //   $congratulationsMessage.html("Unfortunately you lost this round. Click start to try again")
-  //   $displayMessage.html('Score ' + correctClicks);
-  //   $numberOfClicksMade.html('number of clicks made ' + clickCounter);
-  //
-  // }
-
-  else {
-    // $displayMessage.html('Score ' + correctClicks);
-    // $numberOfClicksMade.html('number of clicks made ' + clickCounter);
-  }
-
-}
 
 // this function restets everything each time the button is pressed.
-function resetter(){
-  $congratulationsMessage.html('')
-  $promptToEnter.html('')
-  // $displayMessage.html('');
-  // $numberOfClicksMade.html('');
-  indexCounter = -1;
-  playersChoice = [];
-  computersChoice = [];
-  $('.boxes').css('backgroundColor','white');
-  clickCounter = 0;
-  correctClicks = 0;
-  incorrectClicks = 0;
-
-}
-
-
-
+  function resetter(){
+    $congratulationsMessage.html('')
+    $promptToEnter.html('')
+    // $displayMessage.html('');
+    // $numberOfClicksMade.html('');
+    indexCounter = -1;
+    playersChoice = [];
+    computersChoice = [];
+    $('.boxes').css('backgroundColor','white');
+    clickCounter = 0;
+    correctClicks = 0;
+    incorrectClicks = 0;
+  }
 });
